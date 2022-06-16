b499b9fced56615aefb0b90843dd00d6d11f2f1c
ayutaz <ka1357amnbpdr@gmail.com>
2022-06-16 17:49:18 +0900

    feat: ビルド後にdiscordにバイナリーデータのダウンロードページURLの送信

---
+++ .github/send_download_artifact_url/send-download-artifact-page-url.py
@@ -0,0 +1,35 @@
+import os
+import requests
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+headers = {
+    "Accept": "application/vnd.github.v3+json",
+    "Authorization": os.getenv("PERSONAL_ACCESS_TOKEN"),
+}
+
+req = requests.get(" https://api.github.com/repos/ayutaz/UnityBuildNotificationToDiscord/actions/artifacts",
+                   headers=headers).json()
+
+
+def get_download_url(content):
+    for artifact in content["artifacts"]:
+        if artifact["name"] == "Build-StandaloneWindows64":
+            run_id = artifact["workflow_run"]["id"]
+            url = f"https://github.com/ayutaz/UnityBuildNotificationToDiscord/actions/runs/{run_id}"
+            return url
+    return None
+
+
+def message(url):
+    content = {
+        "username": "ビルドダウンロードページ",
+        "content": "ビルドが終了しました。"
+                   + f"\nダウンロードページ: {url}"
+    }
+    return content
+
+
+requests.post(os.getenv("DISCORD_WEBHOOK_URL"), message(get_download_url(req)))
